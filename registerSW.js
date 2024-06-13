d4c34e81bbdea9dac51746a8bc25881412f3695f
Udo Bauer <knackwurstking@Udos-MacBook-Air.fritz.box>
2024-06-13 10:35:14 +0200

    updete vite cofiguration

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pg-vis.github.io/sw.js', { scope: '/pg-vis.github.io/' })})}
